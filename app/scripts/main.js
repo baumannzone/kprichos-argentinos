4a729645c95b44c295ac32eef03a6b30400d9f52
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-02-07 16:44:00 +0100

    Add slider, add text, add cards, add images, add css styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,23 @@
 var Main = {
   init: function() {
     Main.initMobileNav();
+    Main.homeCarousel();
   },
 
   initMobileNav: function () {
     $('.button-collapse').sideNav();
+  },
+
+
+  // Slick carousel
+  homeCarousel: function () {
+    $('.fotos-slick').slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+      dots: true,
+      mobileFirst: true
+    });
   }
 
 };
