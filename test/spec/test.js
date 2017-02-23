8c563e340cbe82dcfeae8022e7890e5ff4be69fb
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-02-23 01:02:39 +0100

    Add config

--- test/spec/test.js
+++ test/spec/test.js
@@ -4,7 +4,8 @@
   describe('Give it some context', function () {
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
-
+          var title = document.getElementsByTagName('title')[0].innerHTML;
+          (title).should.be.equal('Mocha Spec Runner');
       });
     });
   });
