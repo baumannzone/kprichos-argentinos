40d4ad6ab4fedbd12fa964222fc3a38797eb3d99
Jorge Baumann <jorge.baumann@cloudappi.net>
2017-03-02 10:16:33 +0100

    Cambios

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,49 +1,48 @@
-var Main = {
-  init: function() {
-    Main.initMobileNav();
-    Main.homeCarousel();
-    Main.macy();
-    Main.parallax();
-  },
+(function (  ) {
+  'use strict';
 
-  initMobileNav: function () {
-    $('.button-collapse').sideNav();
-  },
+  var Main = {
+    init: function() {
+      Main.initMobileNav();
+      Main.homeCarousel();
+      Main.macy();
+    },
+
+    initMobileNav: function () {
+      $('.button-collapse').sideNav();
+    },
 
 
-  // Slick carousel
-  homeCarousel: function () {
-    $('.fotos-slick').slick({
-      autoplay: true,
-      autoplaySpeed: 4000,
-      arrows: false,
-      dots: true,
-      mobileFirst: true
-    });
-  },
+    // Slick carousel
+    homeCarousel: function () {
+      $('.fotos-slick').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        dots: true,
+        mobileFirst: true
+      });
+    },
 
-  // Masonry (Products)
-  macy: function () {
-    Macy.init({
-      container: '#macy-cont',
-      trueOrder: false,
-      waitForImages: false,
-      margin: 15,
-      breakAt: {
-        940: 3,
-        650: 2,
-        400: 1
-      }
-    });
-  },
+    // Masonry (Products)
+    macy: function () {
+      Macy.init({
+        container: '#macy-cont',
+        trueOrder: false,
+        waitForImages: false,
+        margin: 15,
+        breakAt: {
+          940: 3,
+          650: 2,
+          400: 1
+        }
+      });
+    }
 
-  parallax: function () {
-    $('.parallax').parallax();
-  }
+  };
 
-};
+  $(function() {
+    Main.init();
+  });
 
-$(function() {
-  Main.init();
-});
-
+})();
