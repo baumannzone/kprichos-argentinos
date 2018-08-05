d9de3e89bbba999e0d12c7976dfbd1c10de776d9
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2018-08-05 10:28:11 +0200

    Fix fonts task

--- gulpfile.js
+++ gulpfile.js
@@ -86,10 +86,9 @@ gulp.task('images', () => {
     .pipe(gulp.dest('docs/images'));
 });
 
-gulp.task('fonts', () => {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('docs/fonts')));
+gulp.task('fonts', function() {
+  return gulp.src([ 'app/fonts/**/'])
+    .pipe(gulp.dest('docs/fonts/'));
 });
 
 gulp.task('extras', () => {
